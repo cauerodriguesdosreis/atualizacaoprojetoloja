b3c1478249345a29c2edcdc9a4052d8883ed42e6
Cauê Rodrigues dos Reis <caue15@outlook.com.br>
2025-04-29 18:40:49 -0300

    Produto.java

--- Produto.java
+++ Produto.java
@@ -1,20 +1,39 @@
-package produtosEcompra;
+package pooLoja;
 
-//Representa um produto vendável
 public class Produto {
- private String nome;
- private double preco;
+	
+	private String nome;
+	private double preco;
+	private int qtd;
 
- public Produto(String nome, double preco) {
-     this.nome = nome;
-     this.preco = preco;
- }
+	public Produto(String nome, double preco, int qtd) {
+		this.nome = nome;
+		this.preco = preco;
+		this.qtd = qtd;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
 
- public String getNome() {
-     return nome;
- }
-
- public double getPreco() {
-     return preco;
- }
+	public double getPreco() {
+		return preco;
+	}
+	
+	public int getQtd() {
+		return qtd;
+	}
+	
+	public int CompraProduto() {
+		return qtd;
+	}
+	
+    public boolean venderUmaUnidade() {
+        if (qtd > 0) {
+            qtd--;
+            return true;
+        } else {
+            return false;
+        }
+    }	
 }
