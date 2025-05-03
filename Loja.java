906c48303d892a2288c36eb9868157609315460a
Cauê Rodrigues dos Reis <caue15@outlook.com.br>
2025-05-03 16:08:43 -0300

    Update Loja.java

--- Loja.java
+++ Loja.java
@@ -12,6 +12,7 @@ public class Loja {
 	private char ComprarNovamente;
 	private boolean MaisCompras;
 	private boolean comprando;
+	private boolean interacao;
 
 	public Loja() {
 		// inicializa o catálogo de produtos
@@ -19,6 +20,7 @@ public class Loja {
 		produtos.add(new Produto("Bolo de chocolate", 15.00, 6));
 		produtos.add(new Produto("Coca-Cola 2L", 12.00, 7));
 		produtos.add(new Produto("Chocolate LACTA - Tamanho Família", 13.00, 9));
+		produtos.add(new Produto("Trakinas - Morango", 7.00, 12));
 	}
 
 	public void run() {
@@ -27,38 +29,52 @@ public class Loja {
 	}
 
 	private void InteraçãoInicialCliente() {
-		// --- 1) Boas-vindas ---
-		System.out.println("Olá! Seja bem-vindo(a) à nossa loja virtual.");
+		interacao = true;
+		
+			// --- 1) Boas-vindas ---
+			System.out.println("Olá! Seja bem-vindo(a) à nossa loja virtual.");
+			
+			while (interacao) {
+			// --- 2) Pergunta se quer comprar ---
+			System.out.print("Você deseja comprar algo? (s/n): ");
+			char resposta = sc.nextLine().trim().toLowerCase().charAt(0);
 
-		// --- 2) Pergunta se quer comprar ---
-		System.out.print("Você deseja comprar algo? (s/n): ");
-		char resposta = sc.nextLine().trim().toLowerCase().charAt(0);
-		if (resposta != 's') {
-			System.out.println("Tudo bem, volte sempre!");
-			return;
+			if (resposta != 's' && resposta != 'n') {
+				System.out.println("Digite sim ou não...");
+			}
+
+			else if (resposta == 's') {
+
+				// --- 3) Pergunta dados do cliente e os armazena ---
+				System.out.println("Qual é seu nome? ");
+				String nome = sc.nextLine();
+
+				System.out.print("Digite seu saldo disponível (R$): ");
+				double saldo = sc.nextDouble();
+				sc.nextLine(); // consome o ENTER restante
+
+				this.cliente = new Cliente(nome, saldo);
+
+				System.out.println("\nCerto, levaremos você ao nosso menu...");
+				
+				interacao = false;
+				ExibirMenu(cliente);
+				
+			} else if (resposta == 'n') {
+				System.out.println("Tudo bem, volte sempre!");
+				return;
+			}
+			
 		}
-
-		// --- 3) Pergunta dados do cliente e os armazena ---
-		System.out.println("Qual é seu nome? ");
-		String nome = sc.nextLine();
-
-		System.out.print("Digite seu saldo disponível (R$): ");
-		double saldo = sc.nextDouble();
-		sc.nextLine(); // consome o ENTER restante
-
-		this.cliente = new Cliente(nome, saldo);
-
-		System.out.println("\nCerto, levaremos você ao nosso menu...");
-
-		ExibirMenu(cliente);
-	}
-
+			
+		}
+		
 	private void ExibirMenu(Cliente cliente) {
 
 		// --- 4) Loop do menu de produtos ---
 		comprando = true;
 		int opcao = 0;
-			 while (comprando) {
+		while (comprando) {
 			System.out.println("\n----- MENU DE PRODUTOS -----");
 			for (int i = 0; i < produtos.size(); i++) {
 				Produto p = produtos.get(i);
@@ -78,10 +94,13 @@ public class Loja {
 			}
 		}
 	}
-	
-	
+
 	// Método de compra após selecionar o produto desejado
 	private void realizarCompra(Produto produto) {
+		if (produto.getQtd() <= 0) {
+			System.out.println("Produto indisponível em estoque.");
+			return;
+		}
 		System.out.println("|");
 		System.out.printf("Você selecionou: %s (R$ %.2f)\n", produto.getNome(), produto.getPreco());
 
@@ -90,32 +109,39 @@ public class Loja {
 			produto.venderUmaUnidade();
 			System.out.printf("Compra realizada! Saldo restante: R$ %.2f\n", cliente.getSaldo());
 			System.out.printf("Quantidade restante do mesmo produto: %d", produto.CompraProduto());
-		} else {
-			System.out.println("Compra não-realizada.");
-			System.out.println("Saldo insuficiente.");
+		}
+		
+		else {
+			System.out.println("Compra não efetuada pois você não possui saldo suficiente.");
+			System.out.println("Volte com mais dinheiro!");
 			comprando = false;
+			MaisCompras = false;
 			return;
 		}
-
+		
+		MaisCompras = true;
 		System.out.println();
 		System.out.println("|");
-		System.out.println("Deseja comprar mais alguma coisa?");
-		ComprarNovamente = sc.next().toLowerCase().charAt(0);
-		MaisCompras = (ComprarNovamente == 's');
-
-		do {
-			if (MaisCompras) {
+		
+		while (MaisCompras) {
+			System.out.println("Deseja comprar mais alguma coisa?");
+			ComprarNovamente = sc.next().toLowerCase().charAt(0);
+			
+			if (ComprarNovamente == 's') {
 				ExibirMenu(cliente);
-			} else if (!MaisCompras) {
+			}
+			else if (ComprarNovamente == 'n') {
 				System.out.println("Obrigado por comprar conosco, " + cliente.getNome() + "!");
+				MaisCompras = false;
 				comprando = false;
 				return;
-			} else {
-				System.out.println("Opção Inválida. Tente novamente.");
 			}
-			
-			
-		} while (ComprarNovamente == 'n');
+			else {
+				System.out.println("Não entendemos. Poderia digitar novamente?");
+				
+			}
+		}
+
 	}
 
 }
